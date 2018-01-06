97572eae6a9831cc928e4d26b3824cb23d37ece7
Luis <luisdcb97@gmail.com>
2018-01-06 21:15:06 +0000

    [#6] Add optimized versions from overview

--- Problems/[#6] Sum square difference/main.py
+++ Problems/[#6] Sum square difference/main.py
@@ -10,5 +10,17 @@ def difference(number):
     return square_of_sum(number) - sum_of_squares(number)
 
 
+def square_of_sum_opt(number):
+    return (number * (number + 1) / 2) ** 2
+
+
+def sum_of_squares_opt(number):
+    return (2 * number + 1) * (number + 1) * number / 6
+
+
+def difference_opt(number):
+    return square_of_sum_opt(number) - sum_of_squares_opt(number)
+
+
 if __name__ == '__main__':
     print(difference(100))
